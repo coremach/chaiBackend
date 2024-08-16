7f6c353d107479af6fd195e15cd6da6a02955139
coremach <ashokshakya0075@gmail.com>
2024-08-16 23:58:44 +0530

    sas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Jai Shree Ram");
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js';
+// import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './env'
+})
+
+// connectDB();
+
+import express from 'express';
+const app = express();
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on ${process.env.PORT}\nDatabase successfully connected !!!`);
+        })
+    }
+    catch (err) {
+        console.error("Error", err)
+        throw err;
+    }
+
+})();
