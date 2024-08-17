290bbe923dce747c7698f915e9ac3bf8894aae05
coremach <ashokshakya0075@gmail.com>
2024-08-17 15:30:18 +0530

    utils code discustion

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from '../constants.js';
 const connectDB = async ()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log(`\n MongoDb connected ~~ DB Host : ${connectionInstance.connection.name}`);
+        console.log(`\n MongoDb connected successfully\n DB name : ${connectionInstance.connection.name}`);
     } catch (error) {
         console.log(`Database connection Error : ${error}`);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants.js';
 import connectDB from './db/index.js';
 import dotenv from 'dotenv';
+import app from './app.js'
 dotenv.config({
     path: './env'
 })
 
-connectDB();
-console.log(process.env.MONGODB_URL);
-
-import express from 'express';
-const app = express();
-; (async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("Error ", error);
-            throw error;
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(` App is listening  on PORT : ${process.env.PORT}`);
         })
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on ${process.env.PORT}\nDatabase successfully connected !!!`);
-        })
-    }
-    catch (err) {
-        console.error("Database Connection Error : ", err)
-        throw err;
-    }
+    })
+    .catch(err => {
+        console.log(" Mongo db connection failed !!! ", err);
+    })
 
-})
-// ();
+// Connection with Database and express server
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants.js';
+// import express from 'express';
+// const app = express();
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error ", error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on ${process.env.PORT}\nDatabase successfully connected !!!`);
+//         })
+//     }
+//     catch (err) {
+//         console.error("Database Connection Error : ", err)
+//         throw err;
+//     }
+
+// })
+// // ();
